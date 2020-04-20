d73afd91287e9fd3094c36fbb9cfdbf88467c67d
João Vitor <34198932+jvitorsl@users.noreply.github.com>
2020-04-19 21:20:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import './main.css';
+import './receita.css';
+import receitas from './receitas.js';
+import favicon from './images/favicon.png';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/receitas/:id">
+            <Cards />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function ListaIngredientes({ ingrediente }) {
+  const list = ingrediente.map(ingrediente => <li>{ingrediente.qtd} {ingrediente.nome}</li>);
+  return (
+    <ul>
+      {list}
+    </ul>
+  );
+}
+
+function ModoPreparo({ preparo }) {
+  const list = preparo.map(passo => <li>{passo}</li>);
+  return (
+    <ol>
+      {list}
+    </ol>
+  );
+}
+
+function CardReceita({ titulo, imagem }) {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      <div className="card-body">
+        <h5 className="card-title" style={{ minHeight: "48px", margin: "0" }}>{titulo}</h5>
+      </div>
+      <div id="card-img" style={{ height: "180px", background: "url(" + imagem + ") center no-repeat", backgroundSize: "cover", borderBottomLeftRadius: "20px", borderBottomRightRadius: "20px" }}>
+        <div id="gradiente"></div>
+      </div>
+    </div>
+  );
+}
+
+function ListaReceitas() {
+  const arr = []
+  for (let i in receitas) {
+    arr.push(
+      <Link id="aCard" to={`/receitas/${i}`}>
+        <CardReceita titulo={receitas[i].titulo} imagem={receitas[i].foto} />
+      </Link>
+    )
+  }
+  return arr
+}
+
+function Home() {
+  return (
+    <>
+      <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@700&display=swap" rel="stylesheet"></link>
+      <div id="main-content">
+        <div id="header">
+          <div id="header-content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '4rem', marginBottom: '4rem' }}>
+
+            <img id="i" src={favicon} />
+
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start', marginLeft: '2rem' }}>
+              <span id="title">Livro de Receitas</span>
+              <span id="stitle">da família Silva Luciano</span>
+            </div>
+          </div>
+        </div>
+
+        <p id="subtitle">Escolha a receita:</p>
+
+        <div id="lista">
+          <ListaReceitas />
+        </div>
+      </div>
+    </>
+  );
+}
+
+function Receita({ titulo, imagem, id, site }) {
+  return (
+    <>
+      <div id="foto" style={{ background: "url(" + imagem + ") center no-repeat", backgroundSize: "cover" }}></div>
+      <div id="receita">
+        <h1 id="titulo">{titulo}</h1>
+        <div id="ingredientes">
+          <h2 id="t2">Ingredientes</h2>
+          <ListaIngredientes ingrediente={receitas[id].ingredientes} />
+        </div>
+
+        <div id="modo">
+          <h2 id="t2">Modo de preparo</h2>
+          <ModoPreparo preparo={receitas[id].preparo} />
+        </div>
+        <p id="site">Retirado de: {site}</p>
+      </div>
+    </>
+  );
+}
+
+function Cards() {
+  let { id } = useParams();
+  return (
+    <Receita site={receitas[id].url} id={id} titulo={receitas[id].titulo} imagem={receitas[id].foto} />
+  );
+}
---
+++ src/receitas.js
@@ -0,0 +1,402 @@
+import torta from './images/torta-joao.png'
+import boloSorvete from './images/bolosorvete.png'
+import crepioca from './images/crepioca.png'
+import crepe from './images/crepe.png'
+import brigadeiro from './images/brigadeiro.png'
+import cookie from './images/cookie-renan.png'
+import lasanha from './images/lasanha-joao.png'
+import tortapizza from './images/tortapizza.jpg'
+
+const receitas = [
+    {
+        titulo: "Bolo de Sorvete",
+        ingredientes: [
+            {
+                qtd: "200ml",
+                nome: "água",
+            },
+            {
+                qtd: "1/2 xícara (chá)",
+                nome: "achocolatado em pó",
+            },
+            {
+                qtd: "1 pitada",
+                nome: "sal",
+            },
+            {
+                qtd: "1/2 colher (chá)",
+                nome: "café solúvel",
+            },
+            {
+                qtd: "1 lata",
+                nome: "leite condensado",
+            },
+            {
+                qtd: "3",
+                nome: "gemas peneiradas",
+            },
+            {
+                qtd: "2 xícaras (chá)",
+                nome: "leite",
+            },
+            {
+                qtd: "3",
+                nome: "claras",
+            },
+            {
+                qtd: "125g",
+                nome: "açúcar",
+            }
+        ],
+        preparo: [
+            "Na panela, coloque a água, o achocolatado, o sal e misture. Leve ao fogo médio até reduzir, por mais ou menos 10 minutos. Apague o fogo, adicione o café solúvel e misture. Transfira para uma forma de furo no meio de 21 centímetros de diâmetro por 8 centímetros de altura e reserve no freezer até esfriar;",
+            "Numa panela, coloque 1 lata de leite condensado, 3 gemas peneiradas, 2 xícaras de chá de leite e misture. Leve ao fogo baixo por 10 minutos mexendo sem parar. Apague o fogo, transfira para uma tigela e reserve até este creme esfriar;",
+            "Em outra panela, coloque as claras, o açúcar e leve ao fogo baixo por 2 minutos ou até derreter o açúcar mexendo sem parar. Apague o fogo e transfira imediatamente para a batedeira. Bata em velocidade baixa por volta de 10 minutos ou até ficar em ponto de merengue. Desligue a batedeira e incorpore o merengue ao creme frio. Transfira para a forma com a calda, cubra com plástico filme e leve ao freezer por 6 horas;",
+            "Depois de 6 horas, retire da geladeira e deixe em temperatura ambiente por 10 minutos ou coloque a forma para esquentar na boca do fogão. Desenforme, decore com amêndoas laminadas e sirva em seguida;",
+            "Se quiser: Decore com amêndoas fatiadas e torradas.",
+        ],
+        foto: boloSorvete,
+        url: "https://gshow.globo.com/receitas-gshow/receitas/bolo-de-sorvete.ghtml"
+    },{
+        titulo: "Brigadeiro",
+        ingredientes: [
+            {
+                qtd: "1 colher (sopa)",
+                nome: "manteiga sem sal",
+            },
+            {
+                qtd: "1 lata",
+                nome: "leite condensado",
+            },
+            {
+                qtd: "1",
+                nome: "gema de ovo peneirada",
+            },
+            {
+                qtd: "1 colher (sopa)",
+                nome: "chocolate em pó peneirado",
+            }
+        ],
+        preparo: [
+            "Primeiro coloque uma colher de sopa de manteiga sem sal numa panela em fogo baixo e deixe até que derreta a manteiga;",
+            "Quando a manteiga estiver quase toda derretida, mas sem ferver, adicione uma lata de leite condensado e uma gema de ovo peneirada. Misture um pouco e junte 1 colher de sopa de chocolate em pó peneirado;",
+            "Em seguida, mexa constantemente, sem parar, com uma colher de pau (ou de silicone) até que o doce solte da panela (quando a passagem da colher deixa atrás dela um rastro que mostra o fundo da panela)."
+        ],
+        foto: brigadeiro,
+        url: "https://gshow.globo.com/receitas-gshow/receitas/brigadeiro-do-william-bonner-55d8b48b4d388573c1000041.ghtml"
+    },{
+        titulo: "Cookies",
+        ingredientes: [
+            {
+                qtd: "1/2 xícara",
+                nome: "açúcar",
+            },
+            {
+                qtd: "3/4 xícara",
+                nome: "açúcar mascavo",
+            },
+            {
+                qtd: "1 colher (chá)",
+                nome: "sal",
+            },
+            {
+                qtd: "1/2 xícara",
+                nome: "manteiga",
+            },
+            {
+                qtd: "1",
+                nome: "ovo",
+            },
+            {
+                qtd: "1 colher (chá)",
+                nome: "essência de baunilha",
+            },
+            {
+                qtd: "11/4 xícaras",
+                nome: "farinha de trigo",
+            },
+            {
+                qtd: "1/2 colher (chá)",
+                nome: "bicarbonato de sódio",
+            },
+            {
+                qtd: "110g",
+                nome: "chocolate meio amargo",
+            },
+            {
+                qtd: "110g",
+                nome: "chocolate ao leite",
+            }
+        ],
+        preparo: [
+            "Coloque os 4 primeiros ingredientes num recipiente e misture até ficar homogêneo;",
+            "Acrescente o ovo e a essência e mexa novamente até ficar homogêneo;",
+            "Peneire a farinha e o bicarbonato, misture com a massa novamente até ficar homogênea;",
+            "Pique os chocolates, misture-os na massa e reserve na geladeira com papel filme (quanto mais tempo, melhor);",
+            "Faça bolinhas com a massa e espalhe com uma distância considerável por uma forma forrada com papel manteiga",
+            "Leve ao forno à 180°C por 12-15 minutos."
+        ],
+        foto: cookie,
+        url: "https://www.facebook.com/watch/?v=1976097799333882"
+    },{
+        titulo: "Crepe suíço",
+        ingredientes: [
+            {
+                qtd: "500ml",
+                nome: "leite",
+            },
+            {
+                qtd: "3",
+                nome: "ovos",
+            },
+            {
+                qtd: "3 colheres (sopa)",
+                nome: "açúcar",
+            },
+            {
+                qtd: "2 colheres (sobremesa)",
+                nome: "sal",
+            },
+            {
+                qtd: "2 xícaras",
+                nome: "amido de milho",
+            },
+            {
+                qtd: "3 xícaras",
+                nome: "farinha de trigo",
+            },
+            {
+                qtd: "2 colheres (sobremesa)",
+                nome: "fermento em pó",
+            }
+        ],
+        preparo: [
+            "Coloque os primeiros ingredientes (líquidos, sal e açúcar);",
+            "Acrescente os demais ingredientes com exceção do fermento em pó;",
+            "Após a massa ficar homogênea e no ponto, coloque o fermento em pó e bata por mais alguns segundos;",
+            "Agora é só colocar a massa na máquina de crepe com o recheio espetado e esperar por [falta ver o tempo].",
+            "OBS: NÃO abra a máquina antes do tempo para não murchar."
+        ],
+        foto: crepe,
+    },{
+        titulo: "Crepioca",
+        ingredientes: [
+            {
+                qtd: "2 colheres (sopa)",
+                nome: "goma de tapioca",
+            },
+            {
+                qtd: "2 colheres (sopa)",
+                nome: "queijo parmesão ralado",
+            },
+            {
+                qtd: "1 pitada",
+                nome: "sal",
+            },
+            {
+                qtd: "1",
+                nome: "ovo",
+            },
+            {
+                qtd: "À gosto:",
+                nome: "orégano ",
+            },
+            {
+                qtd: "À gosto",
+                nome: "sal",
+            },
+            {
+                qtd: "À gosto:",
+                nome: "pimenta do reino",
+            }
+        ],
+        preparo: [
+            "Misture todos os ingredientes numa vasilha;",
+            "Coloque a massa numa frigideira até que dê para virar;",
+            "Preencha com o recheio que quiser e enrole."
+        ],
+        foto: crepioca,
+        url: "https://www.youtube.com/watch?v=mQ8tcndECh4"
+    },/*{
+        titulo: "[EM BREVE] Delícia de Abacaxi: [
+            {
+                qtd: "",
+                nome: "",
+            }
+        ],
+        preparo: [
+        
+        ],
+        foto: semFoto,
+    },{
+        titulo: "[EM BREVE] Hamburguer artesanal do Renan",
+        ingredientes: [
+            {
+                qtd: "",
+                nome: "",
+            }
+        ],
+        preparo: [
+        
+        ],
+        foto: semFoto,
+    },*/{
+        titulo: "Lasanha de frango com queijo",
+        ingredientes: [
+            {
+                qtd: "1",
+                nome: "peito de frango",
+            },
+            {
+                qtd: "500g",
+                nome: "queijo mussarela fatiado",
+            },
+            {
+                qtd: "400g",
+                nome: "presuto fatiado",
+            },
+            {
+                qtd: "1 pacote médio",
+                nome: "massa para lasanha (sem cozimento prévio)",
+            },
+            {
+                qtd: "1 pote",
+                nome: "requeijão cremoso",
+            },
+            {
+                qtd: "2",
+                nome: "caldos de galinha (ou tempero completo sabor galinha)",
+            },
+            {
+                qtd: "2 copos",
+                nome: "leite",
+            },
+            {
+                qtd: "1 caixa",
+                nome: "creme de leite",
+            },
+            {
+                qtd: "2 colheres (sopa)",
+                nome: "farinha de trigo",
+            },
+            {
+                qtd: "3 colheres (sopa)",
+                nome: "manteiga",
+            },
+            {
+                qtd: "1",
+                nome: "cebola média",
+            }
+        ],
+        preparo: [
+            "Molho: Faça um creme homogêneo com as 2 colheres de farinha e 2 colheres de manteiga (reservando 1 colher de manteiga), acrescente o leite, 1 caldo de galinha e mexa constantemente. Retire do fogo e acrescente o creme de leite. Reserve;",
+            "Frango: Cozinhe o peito de frango em água sem óleo. Após cozido, desfie o frango, pique a cebola em pedaços pequenos, coloque em uma panela e doure com a manteiga. Acrescente o frango e o caldo de galinha, depois mexa sempre até o frango ficar totalmente dourado;",
+            "Montagem: Em um refratário, coloque 2 conchas de molho. Faça a base com massa de lasanha, cubra com 1 camada de presunto, 1 de queijo e 1 frango (nessa ordem). Sobre o frango coloque 1 camada de requeijão coloque 2 conchas de molho;",
+            "Cubra o requeijão com 1 camada de presunto, 1 camada de queijo e 1 camada de massa. Coloque molho, e repita esse processo até faltar cerca de 2,5 cm para chegar na borda da assadeira. Para finalizar, cubra a lasanha com muito queijo e requeijão e molho;",
+            "Asse por aproximadamente 20 minutos em fogo baixo."
+        ],
+        foto: lasanha,
+    },{
+        titulo: "Torta de Limão",
+        ingredientes: [
+            {
+                qtd: "300g",
+                nome: "biscoito de maisena",
+            },
+            {
+                qtd: "100g",
+                nome: "manteiga",
+            },
+            {
+                qtd: "2",
+                nome: "leite condensado",
+            },
+            {
+                qtd: "4",
+                nome: "limões grandes",
+            },
+            {
+                qtd: "100ml",
+                nome: "creme de leite",
+            }
+        ],
+        preparo: [
+            "Triture o biscoito no liquidificador ou processador;",
+            "Derreta a manteira e jogue por cima do biscoito e misture;",
+            "Pegue uma forma de fundo removível e acomode bem a massa no centro e nas laterais;",
+            "Asse em forno pré-aquecido à 180°C por 8 minutos e espere esfriar;",
+            "Bata no liquidificador por 4 minutos o leite condensado e o suco do limão;",
+            "Coloque o mousse por cima da base da torta já fria;",
+            "Se quiser pode bater o creme de leite fresco em ponto de chantilly e decorar. Se não, só decore com raspas de limão;",
+            "Deixe gelar por 1 hora."
+        ],
+        foto: torta,
+        url: "https://www.instagram.com/p/B4iGEumglxo/"
+    },{
+        titulo: "Torta-pizza",
+        ingredientes: [
+            {
+                qtd: "4",
+                nome: "ovos",
+            },
+            {
+                qtd: "1/2 xícara (chá)",
+                nome: "óleo",
+            },
+            {
+                qtd: "3 xícaras (chá)",
+                nome: "farinha de trigo",
+            },
+            {
+                qtd: "1 xícara 1/2 (chá)",
+                nome: "leite",
+            },
+            {
+                qtd: "À gosto",
+                nome: "sal",
+            },
+            {
+                qtd: "1 colher (sopa)",
+                nome: "fermento em pó",
+            },
+            {
+                qtd: "1 xícara (chá)",
+                nome: "presunto picado (recheio)",
+            },
+            {
+                qtd: "1",
+                nome: "linguiça calabresa cortada em rodelas (recheio)",
+            },
+            {
+                qtd: "Á gosto:",
+                nome: "rodelas de tomates (recheio)",
+            },
+            {
+                qtd: "3 xícaras (chá)",
+                nome: "muçarela ralada (recheio)",
+            },
+            {
+                qtd: "Á gosto:",
+                nome: "azeitonas verdes",
+            },
+            {
+                qtd: "Á gosto:",
+                nome: "orégano",
+            }
+        ],
+        preparo: [
+            "Triture o biscoito no liquidificador ou processador;",
+            "Derreta a manteira e jogue por cima do biscoito e misture;",
+            "Pegue uma forma de fundo removível e acomode bem a massa no centro e nas laterais;",
+            "Asse em forno pré-aquecido à 180°C por 8 minutos e espere esfriar;",
+            "Bata no liquidificador por 4 minutos o leite condensado e o suco do limão;",
+            "Coloque o mousse por cima da base da torta já fria;",
+            "Se quiser pode bater o creme de leite fresco em ponto de chantilly e decorar. Se não, só decore com raspas de limão;",
+            "Deixe gelar por 1 hora."
+        ],
+        foto: tortapizza,
+        url: "https://youtu.be/ACLmjTWRz2w"
+    }
+]
+
+export default receitas;
