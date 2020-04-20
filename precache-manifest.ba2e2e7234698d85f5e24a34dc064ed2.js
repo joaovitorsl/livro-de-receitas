2146513091bcc8e0a4e886d646bdb54b8d691cfe
João Vitor <joaovitorsilvaluciano@gmail.com>
2020-04-19 22:24:27 -0300

    Updates

---
+++ precache-manifest.ba2e2e7234698d85f5e24a34dc064ed2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61fb51c68603c14703cbac65cf7fe56d",
+    "url": "/livro/index.html"
+  },
+  {
+    "revision": "06c74440052d87e0e765",
+    "url": "/livro/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ccad2a03df65be5f9475",
+    "url": "/livro/static/css/main.b141040f.chunk.css"
+  },
+  {
+    "revision": "06c74440052d87e0e765",
+    "url": "/livro/static/js/2.0d91787d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/livro/static/js/2.0d91787d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccad2a03df65be5f9475",
+    "url": "/livro/static/js/main.b4644fe5.chunk.js"
+  },
+  {
+    "revision": "7c7435fab27ae7e94091",
+    "url": "/livro/static/js/runtime-main.f72121d9.js"
+  },
+  {
+    "revision": "0be892e1b7ed02fc76a2a34af02c6928",
+    "url": "/livro/static/media/background.0be892e1.png"
+  },
+  {
+    "revision": "39e2e805bd0687b448a8b3bf7d00e4f6",
+    "url": "/livro/static/media/bolosorvete.39e2e805.png"
+  },
+  {
+    "revision": "2ac571d1351206dcb119dc746fbe6b82",
+    "url": "/livro/static/media/brigadeiro.2ac571d1.png"
+  },
+  {
+    "revision": "c5d42cc5183dab490d4a6ae843cf84ca",
+    "url": "/livro/static/media/cookie-renan.c5d42cc5.png"
+  },
+  {
+    "revision": "26b4c3936fe97ccb9752a6c6f7bd9d85",
+    "url": "/livro/static/media/crepe.26b4c393.png"
+  },
+  {
+    "revision": "47c26dec5cdef9763f937ff26d23e05b",
+    "url": "/livro/static/media/crepioca.47c26dec.png"
+  },
+  {
+    "revision": "ecc3a368f5da5881962701e9b2bb3bf6",
+    "url": "/livro/static/media/favicon.ecc3a368.png"
+  },
+  {
+    "revision": "0eea3a50a77ffc1ee7eca626b7428b1a",
+    "url": "/livro/static/media/lasanha-joao.0eea3a50.png"
+  },
+  {
+    "revision": "9b546033e21e651fe050d73bfd710e57",
+    "url": "/livro/static/media/torta-joao.9b546033.png"
+  },
+  {
+    "revision": "6d7c9c8e1f115c8c9ee2cfdd7d5bb7f7",
+    "url": "/livro/static/media/tortapizza.6d7c9c8e.jpg"
+  }
+]);
